0f3d3263ebbc3781537d730dd0e02c9882a17f8c
Angga Gantira <angga.gantira@gmail.com>
2021-01-24 15:07:59 +0700

    Tutorial Rajaongkir dengan Laravel, Vue Js dan Tailwind CSS#2 : Integrasi Laravel dengan RajaOngkir

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/provinces', [App\Http\Controllers\RajaOngkirController::class, 'getProvinces']);
+Route::get('/cities/{id}', [App\Http\Controllers\RajaOngkirController::class, 'getCities']);
+Route::post('/checkOngkir', [App\Http\Controllers\RajaOngkirController::class, 'checkOngkir']);
